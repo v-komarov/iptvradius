6ce7d1e0059be5afba1aaef7d1440124ce728d5f
Vladimir Komarov <komarov-krsk@mail.ru>
2019-08-28 09:47:26 +0800

    2448

--- mysql/loadcanals.sh
+++ mysql/loadcanals.sh
@@ -2,7 +2,7 @@
 
 docker cp ./radfreecharge.sql freeradiusdb:/
 docker exec -it freeradiusdb mysql -ufreeradius -pfreeradius -Dradius -e "source /radfreecharge.sql" radius
-docker cp ./iptvcanals.sql freeradiusdb:/
-docker exec -it freeradiusdb mysql -ufreeradius -pfreeradius -Dradius -e "source /iptvcanals.sql" radius
-docker cp ./iptvpackets.sql freeradiusdb:/
-docker exec -it freeradiusdb mysql -ufreeradius -pfreeradius -Dradius -e "source /iptvpackets.sql" radius
+#docker cp ./iptvcanals.sql freeradiusdb:/
+#docker exec -it freeradiusdb mysql -ufreeradius -pfreeradius -Dradius -e "source /iptvcanals.sql" radius
+#docker cp ./iptvpackets.sql freeradiusdb:/
+#docker exec -it freeradiusdb mysql -ufreeradius -pfreeradius -Dradius -e "source /iptvpackets.sql" radius

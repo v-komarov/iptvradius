f9b0fd6d79fe94d89484b407e6db1057755bedce
Vladimir Komarov <komarov-krsk@mail.ru>
2019-09-09 15:07:35 +0800

    other

--- radtest.py
+++ radtest.py
@@ -4,16 +4,22 @@ import radius
 import binascii
 
 
+
+
 mac = '90E6BA8B16C5'
 
 r = radius.Radius('TVsTreAM', host='33.16.2.3')
 
-ip = '{:02X}{:02X}{:02X}{:02X}'.format(*map(int, '239.1.21.3'.split('.')))
 
-print ip
-attrs = {
-         'Framed-IP-Address':binascii.unhexlify(ip)
-         }
 
-print(r.authenticate(mac,mac, attributes=attrs))
+for address in ('239.1.21.3','226.110.10.128'):
+
+    ip = '{:02X}{:02X}{:02X}{:02X}'.format(*map(int, address.split('.')))
+
+    print mac,address
+    attrs = {
+             'Framed-IP-Address':binascii.unhexlify(ip)
+             }
+
+    print(r.authenticate(mac,mac, attributes=attrs))
 
